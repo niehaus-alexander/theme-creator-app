128a192656ea28b12fce60ba066e2dae4821a3b2
Alexander <alexander.niehaus@gmx.net>
2024-04-30 11:58:11 +0200

    create ColorForm layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/Color/ColorForm";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      {/* <ColorForm /> */}
+      <ColorForm />
       {initialColors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
--- src/Components/Color/ColorForm.jsx
+++ src/Components/Color/ColorForm.jsx
@@ -1,3 +1,21 @@
 export default function ColorForm() {
-  return <></>;
+  return (
+    <form>
+      <label htmlFor="name">Role</label>
+      <br />
+      <input type="text" name="role" id="role"></input>
+      <br />
+      <label htmlFor="hex">Hex</label>
+      <br />
+      <input type="text" name="hex" id="hex"></input>
+      <input type="color" name="color" id="color"></input>
+      <br />
+      <label htmlFor="contrast">Contrast Text</label>
+      <br />
+      <input type="text" name="contrast" id="contrast"></input>
+      <input type="color" name="contrast-color" id="contrast-color"></input>
+      <br />
+      <button type="submit">ADD COLOR</button>
+    </form>
+  );
 }
